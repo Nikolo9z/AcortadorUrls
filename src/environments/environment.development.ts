9a0e443ada959fc1fe43a38382031d7762303794
Nicolas Sanchez L. <nsanchezl@chilexpress.cl>
2025-01-18 02:55:36 -0300

    https

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://www.shorturl.somee.com/api/AcortadorURL',
+    apiUrl: 'https://www.shorturl.somee.com/api/AcortadorURL',
     paramApikey: 'ocp-apim-subscription-key',
     apiKey: '520c2dcc674946e3a3359d80023bad8f',
     urlOrigen:'http://localhost:4200/',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://www.shorturl.somee.com/api/AcortadorURL',  // Asegúrate de que este sea el endpoint correcto
+    apiUrl: 'https://www.shorturl.somee.com/api/AcortadorURL',  // Asegúrate de que este sea el endpoint correcto
     paramApikey: 'ocp-apim-subscription-key',  // Si este es el parámetro que usas
     apiKey: '520c2dcc674946e3a3359d80023bad8f',  // Coloca aquí tu clave de API
     urlOrigen:'https://acortadorurls.pages.dev/',
